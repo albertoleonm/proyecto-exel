32881dc32f89686c449e2ccbadd0d120ffe792cf
Alberto Leon <albertoleonm21@gmail.com>
2020-06-15 11:35:06 -0500

    Se agrego el id del logo para que lo haga visible

--- js/change-back-navbar.js
+++ js/change-back-navbar.js
@@ -5,11 +5,25 @@ $(window).scroll(function () {
         $("#menu").removeClass("bg-light");
         $("#menu").removeClass("navbar-light");
         $("#logo-img").addClass("bg-trans");
+        $("#logo-mexicana").addClass("invisible");
+        $("#logo-mexicana-color").removeClass("invisible");
+        $("#logo-mexicana-color").addClass("visible");
+
+        $("#buscar-blanco").addClass("invisible");
+        $("#buscar-color").removeClass("invisible");
+        $("#buscar-color").addClass("visible");
     } else {
         $("#menu").removeClass("navbar-black");
         $("#menu").removeClass("bg-trans");
-        $("#menu").addClass("bg-ligth");
         $("#menu").addClass("navbar-light");
+        $("#menu").addClass("bg-ligth");
         $("#logo-img").removeClass("bg-trans");
+        $("#logo-mexicana").removeClass("invisible");
+        $("#logo-mexicana-color").removeClass("visible");
+        $("#logo-mexicana-color").addClass("invisible");
+
+        $("#buscar-blanco").removeClass("invisible");
+        $("#buscar-color").removeClass("visible");
+        $("#buscar-color").addClass("invisible");
     }
 });
