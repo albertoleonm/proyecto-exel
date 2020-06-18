6794cd3392215f4776556fc63e1cd9d54ebc640d
Alberto Leon <albertoleonm21@gmail.com>
2020-06-18 12:35:36 -0500

    Se agrego el icono del marcador

--- js/google-maps.js
+++ js/google-maps.js
@@ -5,9 +5,9 @@ function initMap() {
         zoom: 15
     });
     var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Exel Mexicana',
+        position: { lat: 19.576712, lng: -99.203147 },
+        title:"Exel Mexicana",
         icon: '../assets/img/map-marker.svg'
-      });
+    });
+    marker.setMap(map);
 }
