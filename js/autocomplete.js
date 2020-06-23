055af5154f40ccad31e7e03e3d38996367e2cccf
Alberto Leon <albertoleonm21@gmail.com>
2020-06-23 17:12:07 -0500

    Se desarrolló y agrego el autocompletado en todas las páginas

---
+++ js/autocomplete.js
@@ -0,0 +1,17 @@
+var tags = [
+    "Bandas",
+    "Turbinas",
+    "Discos de acero",
+    "Discos de pasta",
+    "Bujes, rodanas y retenes",
+    "Partes electrínicas",
+    "Shift kits",
+    "Enfriadores",
+    "Filtros",
+    "Juntas y ligas",
+    "Volantes",
+    "Repuestos de direcciones"
+];
+$("#tags").autocomplete({
+    lookup: tags
+});
